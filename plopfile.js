325068027b2918b6fd54ed28b8a4d17f51040119
Rafael Perozin <rafaelperozin@outlook.com>
2022-03-07 11:22:48 +0000

    build(basic-component): organizar o código

--- plopfile.js
+++ plopfile.js
@@ -1,3 +1,60 @@
+// eslint-disable-next-line quotes
+const translationTemplate = `{{camelCase name}}: {title: '{{name}}'},\n$1`;
+const translationPattern =
+	/(\/\/ PLOP WILL AUTO-IMPORT YOUR COMPONENT TRANSLATION HERE. DO NOT REMOVE)/g;
+
+const createComponent = {
+	type: 'add',
+	path: 'src/components/{{name}}/{{name}}.tsx',
+	templateFile: 'plop-templates/basic-component.tsx.hbs',
+};
+
+const createStyle = [
+	{
+		type: 'add',
+		path: 'src/components/{{name}}/_{{dash-case name}}.style.scss',
+		templateFile: 'plop-templates/component.style.scss.hbs',
+	},
+	{
+		type: 'modify',
+		path: 'src/styles/main.scss',
+		pattern:
+			/(\/\/ PLOP WILL AUTO-IMPORT YOUR COMPONENT STYLE HERE. DO NOT REMOVE)/g,
+		template:
+			// eslint-disable-next-line quotes
+			"@import '../components/{{name}}/{{dash-case name}}.style';\n$1",
+	},
+];
+
+const createTranslations = [
+	{
+		type: 'modify',
+		path: 'src/i18n/locales/en.ts',
+		pattern: translationPattern,
+		template: translationTemplate,
+	},
+	{
+		type: 'modify',
+		path: 'src/i18n/locales/fr.ts',
+		pattern: translationPattern,
+		template: translationTemplate,
+	},
+];
+
+const createBasicComponent = [
+	createComponent,
+	...createStyle,
+	...createTranslations,
+];
+
+const promptQuestions = [
+	{
+		type: 'input',
+		name: 'name',
+		message: 'What is the component name (please, use PascalCase)?',
+	},
+];
+
 module.exports = function (plop) {
 	plop.setHelper('dash-case', function (text) {
 		return text.replace(/[A-Z]/g, (letter, index) =>
@@ -6,51 +63,9 @@ module.exports = function (plop) {
 	});
 
 	plop.setGenerator('basic-component', {
-		description: 'Auto create a basic component',
-		prompts: [
-			{
-				type: 'input',
-				name: 'name',
-				message: 'What is the component name (Please, use PascalCase)?',
-			},
-		],
-		actions: [
-			{
-				type: 'add',
-				path: 'src/components/{{name}}/{{name}}.tsx',
-				templateFile: 'plop-templates/basic-component.tsx.hbs',
-			},
-			{
-				type: 'add',
-				path: 'src/components/{{name}}/_{{dash-case name}}.style.scss',
-				templateFile: 'plop-templates/component.style.scss.hbs',
-			},
-			{
-				type: 'modify',
-				path: 'src/styles/main.scss',
-				pattern:
-					/(\/\/ PLOP WILL AUTO-IMPORT YOUR COMPONENT STYLE HERE. DO NOT REMOVE)/g,
-				template:
-					// eslint-disable-next-line quotes
-					"@import '../components/{{name}}/{{dash-case name}}.style';\n$1",
-			},
-			{
-				type: 'modify',
-				path: 'src/i18n/locales/en.ts',
-				// eslint-disable-next-line quotes
-				template: "{{camelCase name}}: {title: '{{name}}'},\n$1",
-				pattern:
-					/(\/\/ PLOP WILL AUTO-IMPORT YOUR COMPONENT TRANSLATION HERE. DO NOT REMOVE)/g,
-			},
-			{
-				type: 'modify',
-				path: 'src/i18n/locales/fr.ts',
-
-				// eslint-disable-next-line quotes
-				template: "{{camelCase name}}: {title: '{{name}}'},\n$1",
-				pattern:
-					/(\/\/ PLOP WILL AUTO-IMPORT YOUR COMPONENT TRANSLATION HERE. DO NOT REMOVE)/g,
-			},
-		],
+		description:
+			'Auto create the basic component setup with styles and translation',
+		prompts: promptQuestions,
+		actions: createBasicComponent,
 	});
 };
